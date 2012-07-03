965f4a38775db6a7587f589dd22d63198b24dd15
faruk <farukemresahin@gmail.com>
2012-07-03 23:57:50 +0200

    Product of Elements Question

---
+++ ProdoctOfAllElements.py
@@ -0,0 +1,13 @@
+'''
+There is an array A[N] of N integers. You have to compose an array B[N] such 
+that Output[i] will be equal to the product of all the elements of
+A[] except A[i].
+
+Example:
+INPUT:[4, 3, 2, 1, 2]
+OUTPUT:[12, 16, 24, 48, 24]
+
+Solve it without the division operator and in O(n).
+
+'''
+
