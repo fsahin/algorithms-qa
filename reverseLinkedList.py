d41806afe551829c8d24a333ff089df522749aae
unknown <efarsah@.eemea.ericsson.se>
2012-07-04 12:31:13 +0200

    Reverse Linked List

---
+++ reverseLinkedList.py
@@ -0,0 +1,31 @@
+'''
+Reverse a linked list
+'''
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+def printLinkedList(root):
+    while root != None:
+        print root.data
+        root = root.next
+
+def reverseLinkedList(root):
+    pass
+    
+    
+
+
+
+
+faruk = Node("faruk")
+emre = Node("emre")
+sahin = Node("sahin")
+
+faruk.next = emre
+emre.next = sahin
+
+printLinkedList(faruk)
+
