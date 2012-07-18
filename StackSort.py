ce1d98b9c30a684af86457db6938e4db839c72a0
faruk <farukemresahin@gmail.com>
2012-07-18 15:02:05 +0200

    Sort a stack

---
+++ StackSort.py
@@ -0,0 +1,24 @@
+"""
+Write a program to sort a stack in ascending order
+"""
+
+def sortStack(stack):
+    """
+    Use a helper stack to sort the original stack, 
+    Uses a python list as a stack, 
+    """
+    dest = []
+    while len(stack) != 0:
+        tmp = stack.pop()
+        while (len(dest) != 0 and dest[-1] > tmp):
+            stack.append(dest.pop())
+        dest.append(tmp)
+            
+    return dest
+    
+
+stack = [6, 4, 7, 9, 0, -1]
+print sortStack(stack)
+    
+
+
